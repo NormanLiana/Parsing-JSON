1564a1c9d4621cba310209a23a7bdb039525c858
NormanLiana <liananorman@pursuit.org>
2019-08-27 16:16:21 -0400

    adds 3 models

---
+++ Parse-JSON-weather-color-users/Models/ColorModel.swift
@@ -0,0 +1,9 @@
+//
+//  ColorModel.swift
+//  Parse-JSON-weather-color-users
+//
+//  Created by Liana Norman on 8/27/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
---
+++ Parse-JSON-weather-color-users/Models/WeatherModel.swift
@@ -0,0 +1,11 @@
+//
+//  WeatherModel.swift
+//  Parse-JSON-weather-color-users
+//
+//  Created by Liana Norman on 8/27/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
+
+
