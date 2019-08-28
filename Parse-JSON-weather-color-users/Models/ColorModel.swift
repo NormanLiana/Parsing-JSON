e7b7a5138f2f74e76ee5d5882b323ffbb8e2c78c
NormanLiana <liananorman@pursuit.org>
2019-08-28 09:51:38 -0400

    sets up structs in color model to parse json color file

--- Parse-JSON-weather-color-users/Models/ColorModel.swift
+++ Parse-JSON-weather-color-users/Models/ColorModel.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+
+struct Color: Codable {
+    let name: ColorNameWrapper
+    let hex: String
+    let rgb: rgbWrapper
+}
+
+struct rgbWrapper: Codable {
+    let fraction: FractionWrapper
+}
+
+struct FractionWrapper: Codable {
+    let r: Double
+    let g: Double
+    let b: Double
+}
+
+struct ColorNameWrapper: Codable {
+    let value: String
+}
